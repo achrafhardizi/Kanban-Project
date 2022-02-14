02b2b921aa0743cfaa4255bdc2592da258434ae5
akapikachu <74060875+r0gue10@users.noreply.github.com>
2022-02-14 15:53:27 +0100

    Update Sidebar.jsx

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,22 +1,21 @@
-
 //import useState hook to create menu collapse state
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 //import react pro sidebar components
 import {
-  ProSidebar,
-  Menu,
-  MenuItem,
-  SidebarHeader,
-  SidebarFooter,
-  SidebarContent,
+    ProSidebar,
+    Menu,
+    MenuItem,
+    SidebarHeader,
+    SidebarFooter,
+    SidebarContent,
 } from "react-pro-sidebar";
 
 //import icons from react icons
-import { FaList, FaRegHeart } from "react-icons/fa";
-import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
-import { RiPencilLine } from "react-icons/ri";
-import { BiCog } from "react-icons/bi";
+import {FaList, FaRegHeart} from "react-icons/fa";
+import {FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle} from "react-icons/fi";
+import {RiPencilLine} from "react-icons/ri";
+import {BiCog} from "react-icons/bi";
 
 
 //import sidebar css from react-pro-sidebar module and our custom css 
@@ -25,37 +24,35 @@ import "./sidebar.css";
 
 
 const Sidebar = () => {
-  
+
     //create initial menuCollapse state using useState hook
     const [menuCollapse, setMenuCollapse] = useState(false)
 
     //create a custom function that will change menucollapse state from false to true and true to false
-  const menuIconClick = () => {
-    //condition checking to change state from true to false and vice versa
-    setMenuCollapse(!menuCollapse);
-  };
+    const menuIconClick = () => {
+        //condition checking to change state from true to false and vice versa
+        setMenuCollapse(!menuCollapse);
+    };
 
-  return (
-    <>
-      <div id="sidebar">
-          {/* collapsed props to change menu size using menucollapse state */}
-        <ProSidebar collapsed={menuCollapse}>
-          <SidebarContent>
-            <Menu iconShape="square">
-              <MenuItem /*active={true}*/>
-                Sessions
-              </MenuItem>
-            </Menu>
-          </SidebarContent>
-          <SidebarFooter>
-            <Menu iconShape="square">
-              <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
-            </Menu>
-          </SidebarFooter>
-        </ProSidebar>
-      </div>
-    </>
-  );
+    return (
+            <div id="sidebar">
+                {/* collapsed props to change menu size using menucollapse state */}
+                <ProSidebar collapsed={menuCollapse} >
+                    <SidebarContent>
+                        <Menu iconShape="square">
+                            <MenuItem /*active={true}*/>
+                                Sessions
+                            </MenuItem>
+                        </Menu>
+                    </SidebarContent>
+                    <SidebarFooter>
+                        <Menu iconShape="square">
+                            <MenuItem icon={<FiLogOut/>}>Logout</MenuItem>
+                        </Menu>
+                    </SidebarFooter>
+                </ProSidebar>
+            </div>
+    );
 };
 
 export default Sidebar;
