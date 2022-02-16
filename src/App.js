9068789375e12801a90c5fa635a01c559b746f0c
akapikachu <74060875+r0gue10@users.noreply.github.com>
2022-02-16 23:16:59 +0100

    useless commit

--- src/App.js
+++ src/App.js
@@ -7,22 +7,7 @@ function App() {
     return (
         <div className="App ">
             <Navibar/>
-            {/*<Sidebar />*/}
-            <Container fluid style={{width: '100%'}}>
-                <Row>
-                    {/*<Col xs={2}><Sidebar/></Col>*/}
-                    <Col>
-                        <Container fluid>
-                            <Row className='mt-3'>
-                                <Col sm={3}><Sections/></Col>
-                                <Col sm={3}><Sections/></Col>
-                                <Col sm={3}><Sections/></Col>
-                                <Col sm={3}><Sections/></Col>
-                            </Row>
-                        </Container>
-                    </Col>
-                </Row>
-            </Container>
+            <Sidebar />
         </div>
     );
 }
