6ec68c59a5acb135f03436f0e7557dbbf0d21bfd
akapikachu <74060875+r0gue10@users.noreply.github.com>
2022-03-05 17:09:17 +0100

    setting up the routing system

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import './App.css';
-import {Navibar, Section, LoginPanel, Sidebar} from './Components'
+import {Navibar, Section, LoginPanel, Sidebar, Home} from './Components'
 import 'bootstrap/dist/css/bootstrap.css';
 import Task from "./Components/Tasks/Task";
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-function App() {
+const App = () => {
 
-    const secs = [<Task />,
-        <Task />,
-        <Task />,
-        <Task />]
+    const secs = [<Task/>,
+        <Task/>,
+        <Task/>,
+        <Task/>
+    ]
 
     return (
         <section className="App">
-            <Navibar/>
-            <div className="layout">
-                <Section tasks={secs}/>
-                <Section tasks={<Task/>}/>
-                <Section/>
-                <Section/>
-                <Section/>
-                <Section/>
-            </div>
-            {/*<Sidebar />*/}
+            {window.location.href === "http://localhost:3000/" ? null : <Navibar/>}
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path='/' element={<LoginPanel/>}/>
+                    <Route exact path='/home' element={<Home/>}/>
+                    <Route exact path='/section' element={
+                        <div className="layout">
+                            <Section tasks={secs}/>
+                            <Section tasks={<Task/>}/>
+                            <Section/>
+                            <Section/>
+                            <Section/>
+                            <Section/>
+                        </div>}/>
+                </Routes>
+
+            </BrowserRouter>
         </section>
     );
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,14 @@
+/*
+ * Components
+ */
+
 export { default as LoginPanel } from './Login/LoginPanel'
 export { default as Navibar } from './Navibar/Navibar'
 export { default as Sidebar } from './Sidebar/Sidebar'
 export { default as Section } from './Sections/Section'
+
+/*
+ * Pages
+ */
+
+export {default as Home} from './Pages/Home'
