b5c49784a0b2cef4fc04c8c89f4e8953a9c1ec3c
akapikachu <74060875+r0gue10@users.noreply.github.com>
2022-05-16 14:24:05 +0100

    FakeData

---
+++ src/FakeData/DummySection.js
@@ -0,0 +1,18 @@
+const sections = [{
+    id: 1,
+    sectionName: "Incoming Bugs"
+}, {
+    id: 2,
+    sectionName: "In Progress"
+}, {
+    "id": 3,
+    sectionName: "QA"
+}, {
+    id: 4,
+    sectionName: "Ready For Launch"
+}, {
+    id: 5,
+    sectionName: "Live"
+}
+]
+export default sections
