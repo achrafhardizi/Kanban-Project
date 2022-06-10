ea263e711423c7fba61938c9fa794445b1a42d47
akapikachu <74060875+r0gue10@users.noreply.github.com>
2022-06-10 14:22:46 +0100

    commit

--- src/Components/Pages/Profile/Profile.js
+++ src/Components/Pages/Profile/Profile.js
@@ -4,26 +4,33 @@ import {EditProfileModal, Sidebar} from "../../index";
 import {useEffect, useState} from "react";
 import {BsCamera} from "react-icons/bs"
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import Loader from "../../UI/Loader/Loader";
 
 const Profile = () => {
 
 
     const [userData, setUserData] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [notFound, setNotFound] = useState(false);
 
+    let navigate = useNavigate();
 
     useEffect(() => {
-        const userId = localStorage.getItem("id")
-        axios.get(`http://localhost:5000/users/get/${userId}`)
-            .then(res => {
-                console.log(res.data);
-                setUserData(res.data);
-                setLoading(false);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-
+        setTimeout(()=>{
+            const userId = localStorage.getItem("id")
+            axios.get(`http://localhost:5000/users/get/${userId}`)
+                .then(res => {
+                    console.log(res.data);
+                    setUserData(res.data);
+                    setLoading(false);
+                })
+                .catch(err => {
+                    console.log(err);
+                    setNotFound(true);
+                    setLoading(false);
+                })
+        },1000)
     }, []);
 
 
@@ -40,52 +47,63 @@ const Profile = () => {
     return (
         <>
             {
-                loading ?
-                    <p style={{display: "none"}}>Loading</p>
-                    : <div className={styles.page}>
-                        <Sidebar/>
-                        <div className={styles.body}>
-                            <div className={styles.profile}>
-                                <div className={styles["profile__picture"]}>
-                                    <label className={styles["profile__changePic"]}
-                                           htmlFor="profilePic"><BsCamera/></label>
-                                    <input id="profilePic" type="file" onChange={loadProfilePicture}/>
-                                    <img src={profilePicture} alt="user profile"/>
-                                </div>
-                                <div className={styles["profile__name"]}>
-                                    <h1>Nom d'utilisateur</h1>
-                                </div>
-                                <div className={styles["profileInfo__container"]}>
-                                    <div className={styles["profileInfo__card"]}>
-                                        <div className={styles["profileInfo__password"]}>
-                                            <span>Nom d'utilisateur :</span> <p>{userData.username}</p>
-                                        </div>
-                                        <div className={styles["profileInfo__name"]}>
-                                            <span>Prenom :</span> <p>{userData.firstName}</p>
-                                        </div>
-                                        <div className={styles["profileInfo__name"]}>
-                                            <span>Nom :</span> <p>{userData.lastName}</p>
-                                        </div>
-                                        <div className={styles["profileInfo__email"]}>
-                                            <span>Date d'anniversaire :</span> <p>{userData.birthdate}</p>
-                                        </div>
-                                        <div className={styles["profileInfo__email"]}>
-                                            <span>E-mail :</span> <p>{userData.email}</p>
-                                        </div>
-
-                                        <div className={styles["profileInfo__password"]}>
-                                            <span>Mot de passe :</span> <p>{userData.password}</p>
-                                        </div>
-                                    </div>
-                                    <button className={styles["edit__profileInfo"]}
-                                            onClick={() => setShowEditProfileModal(true)}
-                                    >Modifier les informations du profil
+                loading ? <Loader/> :
+                    (notFound ?
+                            <div className={styles.page}>
+                                <div className={styles["d-flex"]}>
+                                    <span className={styles["notFound"]}>Utilisateur non trouvé</span>
+                                    <button onClick={() => {
+                                        navigate('/')
+                                    }}>retour à la connexion
                                     </button>
-                                    <EditProfileModal show={showEditProfileModal} user={userData}/>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                            :
+                            <div className={styles.page}>
+                                <Sidebar/>
+                                <div className={styles.body}>
+                                    <div className={styles.profile}>
+                                        <div className={styles["profile__picture"]}>
+                                            <label className={styles["profile__changePic"]}
+                                                   htmlFor="profilePic"><BsCamera/></label>
+                                            <input id="profilePic" type="file" onChange={loadProfilePicture}/>
+                                            <img src={profilePicture} alt="user profile"/>
+                                        </div>
+                                        <div className={styles["profile__name"]}>
+                                            <h1>Nom d'utilisateur</h1>
+                                        </div>
+                                        <div className={styles["profileInfo__container"]}>
+                                            <div className={styles["profileInfo__card"]}>
+                                                <div className={styles["profileInfo__password"]}>
+                                                    <span>Nom d'utilisateur :</span> <p>{userData.username}</p>
+                                                </div>
+                                                <div className={styles["profileInfo__name"]}>
+                                                    <span>Prenom :</span> <p>{userData.firstName}</p>
+                                                </div>
+                                                <div className={styles["profileInfo__name"]}>
+                                                    <span>Nom :</span> <p>{userData.lastName}</p>
+                                                </div>
+                                                <div className={styles["profileInfo__email"]}>
+                                                    <span>Date d'anniversaire :</span> <p>{userData.birthdate}</p>
+                                                </div>
+                                                <div className={styles["profileInfo__email"]}>
+                                                    <span>E-mail :</span> <p>{userData.email}</p>
+                                                </div>
+
+                                                <div className={styles["profileInfo__password"]}>
+                                                    <span>Mot de passe :</span> <p>{userData.password}</p>
+                                                </div>
+                                            </div>
+                                            <button className={styles["edit__profileInfo"]}
+                                                    onClick={() => setShowEditProfileModal(true)}
+                                            >Modifier les informations du profil
+                                            </button>
+                                            <EditProfileModal show={showEditProfileModal} user={userData}/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    )
             }
         </>
     );
--- src/Components/Sessions/Sessions.jsx
+++ src/Components/Sessions/Sessions.jsx
@@ -4,15 +4,19 @@ import {AddSessionModal, Session} from "../index";
 import {BsPlusLg} from "react-icons/bs"
 import axios from "axios";
 import Loader from "../UI/Loader/Loader";
+import {useNavigate} from "react-router-dom";
 
 
 const Sessions = () => {
 
     const [userData, setUserData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [notFound, setNotFound] = useState(false);
+
+    let navigate = useNavigate();
 
     useEffect(() => {
-        setTimeout(()=>{
+        setTimeout(() => {
             const userId = localStorage.getItem("id")
             axios.get(`http://localhost:5000/users/get/${userId}`)
                 .then(res => {
@@ -22,8 +26,10 @@ const Sessions = () => {
                 })
                 .catch(err => {
                     console.log(err);
+                    setNotFound(true);
+                    setLoading(false);
                 })
-        },1000)
+        }, 1000)
 
     }, []);
 
@@ -38,28 +44,42 @@ const Sessions = () => {
         <>
             {
                 loading ? <Loader/> :
-                <div className={styles.page}>
-                    <div className={styles.container}>
-                        {
-                            userData["joined_sessions"].map((e) => (
-                                <Session session={e}
-                                         user = {userData}
-                                         id={e.idSession}
-                                         owned={false} key={e.idSession}/>
-                            ))
-                        }
-                        {
-                            userData["owned_sessions"].map((e) => (
-                                <Session session={e}
-                                         user={userData}
-                                         id={e.idSession}
-                                         owned={true} key={e.idSession}/>
-                            ))
-                        }
-                    </div>
-                    <button className={styles["add__session"]} onClick={addSessionHandler}><BsPlusLg/></button>
-                    <AddSessionModal show={showAddSessionModal} closeModal={setShowAddSessionModal} user={userData}/>
-                </div>
+                    (notFound ?
+                            <div className={styles.page}>
+                                <div className={styles["d-flex"]}>
+                                    <span className={styles["notFound"]}>Utilisateur non trouvé</span>
+                                    <button onClick={()=>{
+                                        navigate('/')
+                                    }}>retour à la connexion</button>
+                                </div>
+                            </div>
+                            :
+                            <div className={styles.page}>
+                                <div className={styles.container}>
+                                    {
+                                        userData["joined_sessions"].map((e) => (
+                                            <Session session={e}
+                                                     user={userData}
+                                                     id={e.idSession}
+                                                     owned={false} key={e.idSession}/>
+                                        ))
+                                    }
+                                    {
+                                        userData["owned_sessions"].map((e) => (
+                                            <Session session={e}
+                                                     user={userData}
+                                                     id={e.idSession}
+                                                     owned={true} key={e.idSession}/>
+                                        ))
+                                    }
+                                </div>
+                                <button className={styles["add__session"]} onClick={addSessionHandler}><BsPlusLg/>
+                                </button>
+                                <AddSessionModal show={showAddSessionModal} closeModal={setShowAddSessionModal}
+                                                 user={userData}/>
+                            </div>
+                    )
+
             }
         </>
 
